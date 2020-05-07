21bef9f8aec761f447da9110fa4bf21cd4095d1d
conorwalker <conor.walk@gmail.com>
2020-05-07 12:35:28 +0100

    Given an input multiple alignment with the MN908947.3 in the first record, update the problematic sites vcf to include all alternative alleles using amiguity coding

--- src/parse_reference_to_vcf.py
+++ src/parse_reference_to_vcf.py
@@ -1,28 +1,82 @@
 #!/usr/local/bin/python
-from Bio import SeqIO
+
+import numpy as np
+from Bio import SeqIO, AlignIO
 from six.moves import urllib
+from collections import Counter
+from sys import argv
+
 
 def main():
+    # parse alignment
+    multiple_alignment = argv[1]
+    records = AlignIO.read(multiple_alignment, "fasta")
+    seq_matrix = []
+    
+    # get reference sequence from alignment
+    ref_align_seq = list(str(records[0].seq).upper())
+
+    # append all seqs to sequence matrix
+    for rec in records:
+        seq_matrix.append(list(str(rec.seq).upper()))
+    seq_matrix = np.asarray(seq_matrix)
+
+    # store alt bases at each site
+    bases_per_ref_site = []
+
+    # determine all bases in each alignment column (slow, works as needed)
+    for i, ref_base in enumerate(ref_align_seq):
+        if ref_base == "-":
+            continue
+        else:
+            bases_at_position = list(Counter(seq_matrix[:,i]).keys())
+            if ref_base in bases_at_position:
+                bases_at_position.remove(ref_base)
+            if "-" in bases_at_position:
+                bases_at_position.remove("-")
+            if "N" in bases_at_position:
+                bases_at_position.remove("N")
+            bases_per_ref_site.append(",".join(bases_at_position))
+
     # parse MN908947.3 sequence
-    ref_fasta = "./SARS-CoV-2.fa"
+    ref_fasta = "SARS-CoV-2.fa"
     ref_seq = str(SeqIO.read(ref_fasta, "fasta").seq)
+
     # retrieve vcf
     cov_vcf = urllib.request.urlopen("https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf")
+
     # hold ref positions
     vcf_lines = []
+
+    # parse vcf from github
+    vcf_contents = cov_vcf.read().decode("utf-8").split("\n")
+
+    # don't report alt variants if in genome end
+    genome_ends = [i+1 for i in range(55)] + [i+1 for i in range(29803,29903)]
+
     # parse ref positions
-    for vcf_line in cov_vcf.read().decode("utf-8").split("\n"):
+    for vcf_i, vcf_line in enumerate(vcf_contents):
+        print("vcf_line: ", vcf_line)
+        if not vcf_line:
+            continue
         if not vcf_line.startswith("#"):
             vcf_line = vcf_line.split("\t")
-            ref_base = ref_seq[int(vcf_line[1])-1]
+            ref_position = int(vcf_line[1])
+            ref_base = ref_seq[ref_position-1]
             vcf_line[3] = ref_base
+            if int(vcf_line[1]) in genome_ends:
+                vcf_line[4] = "."
+            else:
+                vcf_line[4] = bases_per_ref_site[ref_position-1]
             vcf_lines.append("\t".join(vcf_line))
         else:
             vcf_lines.append(vcf_line)
+
     # write updated VCF
-    with open("problematic_sites_sarsCov2_ref.vcf", "w") as out_file:
-        for line in vcf_lines:
-            out_file.write(line + "\n")
+    with open("problematic_sites_sarsCov2.vcf", "w") as out_file:
+        fi_content = "\n".join(vcf_lines)
+        out_file.write(fi_content) 
+
 
 if __name__ == "__main__":
     main()
