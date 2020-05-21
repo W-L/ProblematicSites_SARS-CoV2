aefc2e3b42130a3bbf7e3594814a511088a951ef
conorwalker <conor.walk@gmail.com>
2020-05-21 17:21:57 +0100

    Added script to sort, bgzip and tabix index problematic_sites_sarsCov2.vcf

---
+++ src/bgzip_problematic_sites_vcf.sh
@@ -0,0 +1,12 @@
+## Sort the file on column 2 at non-header lines only
+## standard tools e.g. "bcftools sort" not used as a couple of the lines
+## have non-standard formatting that throw errors. Can be changed easily
+## at the cost of human readability at any point if needed.
+
+# get number of header lines in VCF file
+header_lines=$(grep "#" ../problematic_sites_sarsCov2.vcf | wc -l);
+body_lines=$((header_lines+1))
+
+(head -n "$header_lines" ../problematic_sites_sarsCov2.vcf && tail -n +"$body_lines" ../problematic_sites_sarsCov2.vcf | sort -k2 -n) | bgzip -c > ../problematic_sites_sarsCov2.vcf.gz
+tabix -p vcf ../problematic_sites_sarsCov2.vcf.gz
+tabix --csi ../problematic_sites_sarsCov2.vcf.gz
