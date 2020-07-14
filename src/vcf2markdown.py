d7ec263f553102c735ce3bf7ca5e6074f3d01306
conorwalker <conor.walk@gmail.com>
2020-07-14 12:46:33 +0100

    Split reasons for exclusion by new lines to reduce table width. Accounted for additional VCF columns in seq_end.

--- src/vcf2markdown.py
+++ src/vcf2markdown.py
@@ -16,6 +16,7 @@ def readCustomVCF(fname, keep_cols):
                 data_line = line.rstrip('\n').split(sep='\t')
                 # but skip the ones of the sequence ends
                 if "seq_end" not in data_line:
+                    data_line[8] = "<br>".join(data_line[8].split(","))
                     data.append([data_line[i] for i in keep_cols])
             else:
                 break
@@ -27,15 +28,15 @@ def main():
 
     # name of the vcf file to parse and which columns to select from the file
     filename = "problematic_sites_sarsCov2.vcf"
-    keep = [1, 3, 4, 6, 8, 10, 11, 12, 13]
+    keep = [1, 3, 4, 6, 8, 11, 12, 13, 14]
     # title of the table - printed as markdown header before the table
     writer.table_name = "Human-friendly version of the vcf file"
 
     header, data = readCustomVCF(fname=filename, keep_cols=keep)
     # the markdown table does not need a single line for every site at the ends of the sequence
     # so insert the beginning and append the ends
-    data.insert(0, ["1-55", ".", ".", "seq_end",".", "."])
-    data.append(["29804-29903", ".", ".", "seq_end",".", "."])
+    data.insert(0, ["1-55", ".", ".", "mask","seq_end",".", ".",".","."])
+    data.append(["29804-29903", ".", ".", "mask","seq_end",".",".",".","."])
 
     writer.headers = header
     writer.value_matrix = data
