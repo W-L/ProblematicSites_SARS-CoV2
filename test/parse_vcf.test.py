2eabe68403b76673db66fec5c150a7aa9b48c323
conorwalker <conor.walk@gmail.com>
2020-07-24 11:36:21 +0100

    Added bcftools to dependencies

--- test/parse_vcf.test.py
+++ test/parse_vcf.test.py
@@ -8,12 +8,12 @@ from subprocess import Popen, PIPE
 class TestVCF(unittest.TestCase):
     def test_pyvcf(self):
         try:
-            with open("problematic_sites_sarsCov2.vcf", "r") as vcf_fi:
+            with open("test/problematic_sites_sarsCov2.vcf", "r") as vcf_fi:
                 vcf_reader = vcf.Reader(vcf_fi)
         except SyntaxError:
             self.fail("Error when parsing the new VCF using PyVCF")
     def test_bcftools(self):
-        p = Popen(["bcftools", "view", "{0}".format("problematic_sites_sarsCov2.vcf")], stdout=PIPE, stderr=PIPE)
+        p = Popen(["bcftools", "view", "{0}".format("test/problematic_sites_sarsCov2.vcf")], stdout=PIPE, stderr=PIPE)
         output, error = p.communicate()
         assert len(error) == 0
 
